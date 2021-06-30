844041329ea30afe0ceedfd48c6fc3e072f59212
Matt Millican <matt@mattmillican.com>
2021-06-29 21:33:38 -0500

    Link up tracker side requirement nav links

--- src/views/achievements/Tracker.vue
+++ src/views/achievements/Tracker.vue
@@ -6,9 +6,9 @@
       <div class="row">
         <div class="col-md-4">
           <div class="list-group">
-            <button  class="list-group-item list-group-item-action" :class="{ active: requirementId == step.id }" v-for="step in currentAchievementSteps" :key="step.id">
+            <router-link :to="{ name: 'achievementTracker', params: { achievementId, requirementId: step.id } }"  class="list-group-item list-group-item-action" :class="{ active: requirementId == step.id }" v-for="step in currentAchievementSteps" :key="step.id">
               {{ step.name }}
-            </button>
+            </router-link>
           </div>
         </div>
         <div class="col-md-8">
